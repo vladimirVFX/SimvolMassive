8587a8bee0f29724afdf1464ac04a8e9e5cee4bc
VladimirMironov <44780782+vladimirVFX@users.noreply.github.com>
2020-04-28 19:07:04 +0300

    Add files via upload

---
+++ SimvolMassive.cpp
@@ -0,0 +1,28 @@
+﻿// SimvolMassive.cpp
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    system("chcp 1251 > nul");
+    char str[100] = "Coding coding test string...";
+    cout << str << endl;
+
+    for (int k = 0; str[k]; k++) {
+        cout << str[k] << "_";
+    }
+    cout << endl;
+
+    for (char* p = str; *p; p++) {
+        cout << p << endl;
+    }
+
+
+
+    system("pause > nul");
+    return 0;
+}
+
